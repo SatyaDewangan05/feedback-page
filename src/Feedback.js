a3edefbd4c482b26ef9ea45b8d345626aea28cd6
Satya Dewangan <satyadewangan05@gmail.com>
2022-05-30 23:53:26 +0530

    first commit

---
+++ src/Feedback.js
@@ -0,0 +1,23 @@
+import React from "react";
+
+const Feedback = () => {
+  return (
+    <div className="feedback">
+      <div className="navbar">
+        <div className="navbar-first">
+          <div className="logo">
+            <img src="" alt="" />
+          </div>
+          <div className="nav-list"></div>
+          <div className="search">
+            <i class="fa-solid fa-magnifying-glass"></i>
+            <button>Wallet connect</button>
+          </div>
+          <div className="mode"></div>
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export default Feedback;
